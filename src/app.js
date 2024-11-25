8af01f3e2ddbb9a3fb243a43f6a1f344fa05b768
Animesh Singh <animeshsingh75@gmail.com>
2024-11-24 23:58:19 -0700

    Routes in express.js

--- src/app.js
+++ src/app.js
@@ -2,16 +2,35 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send({
+    first_name: "Animesh",
+    last_name: "Singh",
+  });
+});
+
+app.get("/user/:user_id", (req, res) => {
+  console.log(req.params);
+  res.send({
+    first_name: "Animesh",
+    last_name: "Singh",
+  });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data has been saved successfully!");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Data has been updated successfully!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data has been deleted successfully!");
+});
 app.use("/test", (req, res) => {
   res.send("Hello World!");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello from hello");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello from home!");
 });
 
 app.listen(7777, () => {
