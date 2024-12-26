7648cba2b163db93f02e775bb6aac21c63388e3d
Animesh Singh <animeshsingh75@gmail.com>
2024-12-26 14:22:24 +0530

    Add user feed route to retrieve users excluding connection request participants

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
+const User = require("../models/user");
 const ConnectionRequest = require("../models/connectionRequest");
 
 const userRouter = express.Router();
@@ -46,4 +47,35 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   }
 });
 
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+    const users = await User.find({
+      $and: [
+        { _id: { $ne: loggedInUser._id } },
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+      ],
+    })
+      .select(USER_SAFE_FIELDS)
+      .skip(skip)
+      .limit(limit);
+    res.json({ message: "Feed api", data: users });
+  } catch (err) {
+    res.status(400).json({ message: "Something went wrong" + err.message });
+  }
+});
 module.exports = userRouter;
